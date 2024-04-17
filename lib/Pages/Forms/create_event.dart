b2973998d6278e324c5cdd4f5abf8d9d55285ebd
joshua jabat <joshuajabat@gmail.com>
2024-04-17 17:37:05 +0800

    added forms ui

--- lib/Pages/Forms/create_event.dart
+++ lib/Pages/Forms/create_event.dart
@@ -35,7 +35,6 @@ class _CreateEventPageState extends State<CreateEventPage> {
   if (location != null) {
     setState(() {
       _eventLocation = location;
-      print('Selected location: $_eventLocation');
     });
   }
 }
@@ -96,120 +95,329 @@ class _CreateEventPageState extends State<CreateEventPage> {
       appBar: AppBar(
         title: const Text('Create Event'),
       ),
-      body: Padding(
+      body: 
+      SingleChildScrollView(child: 
+      
+      Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _eventTitleController,
-                decoration: const InputDecoration(
-                  hintText: 'Event Title',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an event title';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                controller: _eventDescriptionController,
-                decoration: const InputDecoration(
-                  hintText: 'Event Description',
-                ),
-                maxLines: 3,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an event description';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              Row(
+
+        child: Container(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Show a date/time picker for the start datetime
-                      },
-                      child: Text('Start: $_startDateTime'),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5), // Shadow color
+                          spreadRadius: 5, // Spread radius
+                          blurRadius: 7, // Blur radius
+                          offset: Offset(0, 3), // Offset
+                        ),
+                      ],
+                    ),
+                    padding: EdgeInsets.all(25),
+                    child: Column(children: [
+                      // Change this 
+                      TextFormField(
+                        controller: _eventTitleController,
+                        decoration: const InputDecoration(
+                          hintText: 'Event Title',
+                          border: InputBorder.none, // Remove the underline
+                          hintStyle: TextStyle(color: Colors.green)
+                        ),
+                        style: TextStyle(color: Colors.green),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an event title';
+                          }
+                          return null;
+                        },
+                      ),
+
+                      Divider(),
+
+
+                      TextFormField(
+                        controller: _eventDescriptionController,
+                        decoration: const InputDecoration(
+                          hintText: 'Event Description',
+                          border: InputBorder.none, // Remove the underline
+                          hintStyle: TextStyle(color: Colors.green)
+
+                        ),
+                        style: TextStyle(color: Colors.green),
+
+                        maxLines: 3, //CHANGE THIS
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an event description';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
                     ),
                   ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Show a date/time picker for the end datetime
-                      },
-                      child: Text('End: $_endDateTime'),
+                  
+                  const SizedBox(height: 16.0),
+                
+
+                  Container(
+                    decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5), // Shadow color
+                              spreadRadius: 5, // Spread radius
+                              blurRadius: 7, // Blur radius
+                              offset: Offset(0, 3), // Offset
+                            ),
+                          ],
+                      ),
+                    child: Column(children: [
+
+
+                        Padding(padding: EdgeInsets.all(10),
+                        
+                          child: Row(
+                          children: [
+                              Icon(Icons.calendar_month, color: Colors.green,), // Example icon
+                              SizedBox(width: 8), // Adjust the spacing between the icon and text
+                              Text(
+                                'Event Date & Time',
+                                style: TextStyle(fontSize: 16, color: Colors.green), // Example text style
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Divider(),
+                        
+                        Padding (
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          child: Row(
+                          children: [
+                            Text('Start:'),
+                            SizedBox(width: 10),
+                            Expanded( child: ElevatedButton(
+                              onPressed: () {}, 
+                              child: Text("June 23, 2024", 
+                                  style: TextStyle(color: Color.fromRGBO(45, 143, 72, 1),
+                                  fontWeight: FontWeight.bold
+                                ),
+                                
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey.withOpacity(0)
+                              ),
+                            ),),
+                            SizedBox(width: 10,),
+                            Expanded( child: ElevatedButton(
+                              onPressed: () {}, 
+                              child: Text("9:00 PM", 
+                                  style: TextStyle(color: Color.fromRGBO(45, 143, 72, 1),
+                                  fontWeight: FontWeight.bold)),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey.withOpacity(0)
+                              ),
+                            ),
+                            ),
+
+                            
+                            ],
+                          )
+                        ),
+
+                        Padding (
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          child: Row(
+                          children: [
+                            Text('End:'),
+                            SizedBox(width: 15,),
+                            Expanded( child: ElevatedButton(
+                              onPressed: () {}, 
+                              child: Text("June 23, 2024", 
+                                  style: TextStyle(color: Color.fromRGBO(45, 143, 72, 1),
+                                  fontWeight: FontWeight.bold
+                                ),
+                                
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey.withOpacity(0)
+                              ),
+                            ),),
+                            SizedBox(width: 10,),
+                            Expanded( child: ElevatedButton(
+                              onPressed: () {}, 
+                              child: Text("9:00 PM", 
+                                  style: TextStyle(color: Color.fromRGBO(45, 143, 72, 1),
+                                  fontWeight: FontWeight.bold)),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey.withOpacity(0)
+                              ),
+                            ),
+                            ),
+
+                            
+                            ],
+                          )
+                        ),
+                    ],)
+                    
+          
+
+                  ),
+
+                  
+
+
+                  const SizedBox(height: 16.0),
+
+
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
                     ),
+                    child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white
+                              ),
+                    onPressed: _pickEventLocation,
+                    child: Padding(padding: EdgeInsets.all(10),
+                        
+                          child: Row(
+                          children: [
+                              Icon(Icons.location_pin, color: Colors.green,), // Example icon
+                              SizedBox(width: 8), // Adjust the spacing between the icon and text
+                              Text(
+                                'Pick a Location',
+                                style: TextStyle(fontSize: 16, color: Colors.green), // Example text style
+                              ),
+                            ],
+                          ),
+                        ),
+                    ),
+                  ),
+                  
+
+
+
+
+                //EVENT TYPE and ORGANIZATION
+                  const SizedBox(height: 16.0),
+
+
+
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        _schoolOrganization = value;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'School/Organization',
+                      border: InputBorder.none, // Remove the underline
+                    ),
+                    validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a school or organization';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+
+
+                  const SizedBox(height: 16.0),
+                  
+                  Container(
+                    
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: DropdownButtonFormField<String>(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    value:  _eventType.isNotEmpty ? _eventType : null,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _eventType = value ?? '';
+                      });
+                    },
+                    items: const [
+                      DropdownMenuItem(value: 'Workshop', child: Text('Workshop')),
+                      DropdownMenuItem(value: 'Meetup', child: Text('Meetup')),
+                      DropdownMenuItem(value: 'Conference', child: Text('Conference')),
+                    ],
+                    decoration: const InputDecoration(
+                      hintText: 'Event Type',
+                      border: InputBorder.none, // Remove the underline
+
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select an event type';
+                      }
+                      return null;
+                    },
+                  ),
+                  ),
+
+
+
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: _pickEventImage,
+                    child: const Text('Upload Event Image'),
+                  ),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: _saveEvent,
+                    child: const Text('Save Event'),
                   ),
                 ],
               ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _pickEventLocation,
-                child: const Text('Pick Event Location'),
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                onChanged: (value) {
-                  setState(() {
-                    _schoolOrganization = value;
-                  });
-                },
-                decoration: const InputDecoration(
-                  hintText: 'School/Organization',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a school or organization';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              DropdownButtonFormField<String>(
-                value: _eventType,
-                onChanged: (value) {
-                  setState(() {
-                    _eventType = value ?? '';
-                  });
-                },
-                items: const [
-                  DropdownMenuItem(value: 'Workshop', child: Text('Workshop')),
-                  DropdownMenuItem(value: 'Meetup', child: Text('Meetup')),
-                  DropdownMenuItem(value: 'Conference', child: Text('Conference')),
-                ],
-                decoration: const InputDecoration(
-                  hintText: 'Event Type',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please select an event type';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _pickEventImage,
-                child: const Text('Upload Event Image'),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _saveEvent,
-                child: const Text('Save Event'),
-              ),
-            ],
           ),
-        ),
+        )
       ),
+      )
+      
     );
   }
 }
