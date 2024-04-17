faf3c3740f6b5ebfbc3bee6d606dd3d9e6329efb
jacegonzales25 <jacegonzales25@gmail.com>
2024-04-17 18:27:09 +0800

    feat: add alternate path for weather

--- lib/Pages/Weather/weather.dart
+++ lib/Pages/Weather/weather.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:weather/weather.dart';
 import 'package:intl/intl.dart';
 
-class Weather extends StatefulWidget{
+class Weather extends StatefulWidget {
   const Weather({super.key});
 
   @override
   State<Weather> createState() => _WeatherState();
 }
-
 
 class _WeatherState extends State<Weather> {
   final WeatherFactory _wf = WeatherFactory("_apiKey");
@@ -20,14 +19,16 @@ class _WeatherState extends State<Weather> {
     super.initState();
     _wf.currentWeatherByLocation(13, 122).then((w) {
       setState(() {
-        _weather = w as Weather;
+        _weather = w as Weather?;
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _buildUI(),);
+    return Scaffold(
+      body: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
@@ -38,16 +39,56 @@ class _WeatherState extends State<Weather> {
       width: MediaQuery.sizeOf(context).width,
       height: MediaQuery.sizeOf(context).height,
       child: Column(
-        children: [],
+        children: [
+          _dateTimeInfo(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.05,
+          ),
+          _weatherIcon(),
+        ],
       ),
     );
   }
 
   Widget _dateTimeInfo() {
     DateTime now = DateTime.now();
-    return Column(children: [
-      Text(DateFormat)
-    ],);
+    return Column(
+      children: [
+        Text(DateFormat("h:mm a").format(now),
+            style: const TextStyle(fontSize: 35)),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(DateFormat("EEEE").format(now),
+                style: const TextStyle(fontSize: 35)),
+            Text(" ${DateFormat("d.m.y").format(now)}",
+                style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                ))
+          ],
+        )
+      ],
+    );
   }
 
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      "http://openweathermap.org/img/wn/${_weather?._weatherIcon}"))),
+        )
+      ],
+    );
+  }
 }
